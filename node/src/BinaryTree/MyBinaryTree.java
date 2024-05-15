cff9f360b7e491f37cc55c813d9d07dca15b4853
Nguyen Thanh Nhat <nguyennhat082004@gmail.com>
2024-05-15 09:52:12 +0700

    add the code

--- node/src/BinaryTree/MyBinaryTree.java
+++ node/src/BinaryTree/MyBinaryTree.java
@@ -8,33 +8,53 @@ public class MyBinaryTree{
 
    public void createTree()
    {
+      TNode A = new TNode(7, new TNode(2), new TNode(6));
+      TNode B = new TNode(5, null, new TNode(9));
+      root = new TNode(2, A, B);
+   }
+   public void duyet1(TNode T)
+   {
+      if (T != null) {
+         System.out.println(T.data);
+         duyet1(T.left);
+         duyet1(T.right);
+      }
 
    }
-   public void tienTu()
+   public void duyet2(TNode T)
    {
-
+      if (T != null) {
+         duyet2(T.left);
+         System.out.println(T.data);
+         duyet2(T.right);
+         
+      }
    }
-   public void trungTu()
+   public void duyet3(TNode T)
    {
-
+      if (T != null) {
+         duyet2(T.left);
+         duyet2(T.right);
+         System.out.println(T.data);
+      }
    }
-   public void hauTu()
+   public void duyetTienTu()
    {
-
+       duyet1(root);
    }
-   public void chuyen1()
+   public void duyetTrungTu()
    {
-
+       duyet2(root);
    }
-   public void chuyen2()
+   public void duyetHauTu()
    {
-    
-   }
-   public void chuyen3()
-   {
-    
+       duyet3(root);
    }
    public static void main(String[] args) {
-    System.out.println("Hello");
+    MyBinaryTree tree = new MyBinaryTree();
+    tree.createTree();
+   //  tree.duyetTienTu();
+   //  tree.duyetTrungTu();
+    tree.duyetHauTu();
    }
 }
